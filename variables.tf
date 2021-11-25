6fec136444635c8cc97e2b021762b28d3203483b
Kevin Leung <kevinresol@gmail.com>
2021-11-25 15:24:16 +0800

    Add description to variables

--- variables.tf
+++ variables.tf
@@ -1,24 +1,38 @@
 variable "name" {
-  type    = string
-  default = "keycloak"
+  description = "Base name for all the created resources"
+  type        = string
+  default     = "keycloak"
 }
 
 variable "image" {
-  type    = string
-  default = "docker.io/jboss/keycloak:latest"
+  description = "Docker image for the Keycloak container"
+  type        = string
+  default     = "docker.io/jboss/keycloak:latest"
 }
 
 variable "namespace" {
-  type    = string
-  default = "default"
+  description = "Namespace to spawn all the resources"
+  type        = string
+  default     = "default"
 }
 
 variable "replicas" {
-  type    = number
-  default = 1
+  description = "Number of replicas for Keycloak. Ignored when autoscaling is specified."
+  type        = number
+  default     = 1
+}
+
+variable "autoscaling" {
+  description = "Enable auto-scaling via horizontal pod autoscaler"
+  type = object({
+    min_replicas = number
+    max_replicas = number
+  })
+  default = null
 }
 
 variable "ingress" {
+  description = "Ingress configurations"
   type = object({
     annotations = optional(map(string))
     # rules = array({
@@ -32,20 +46,14 @@ variable "ingress" {
   default = {}
 }
 
-variable "autoscaling" {
-  type = object({
-    min_replicas = number
-    max_replicas = number
-  })
-  default = null
-}
-
 variable "env" {
-  type    = map(string)
-  default = {}
+  description = "Environment variables for the Keycloak container"
+  type        = map(string)
+  default     = {}
 }
 
 variable "startup_scripts" {
-  type    = map(string)
-  default = {}
+  description = "Startup scripts to be run when the Keycloak container is initalized"
+  type        = map(string)
+  default     = {}
 }
